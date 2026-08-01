f5b0996bb6c9189505baf1ca3e79a1b8af88054d
hcy <hcy@hs-MacBook-Pro-2.local>
2026-08-01 13:48:51 +0800

    feat: configure shadcn/ui, fix default route redirect from /dashboard to /home

---
+++ app/home/page.tsx
@@ -0,0 +1,10 @@
+
+import { Button } from "@/components/ui/button"
+ 
+export default function Page() {
+  return (
+    <div className="flex min-h-svh items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  )
+}
